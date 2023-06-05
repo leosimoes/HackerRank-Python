b2e8c1010c57c3185e9caf1d733a977dbad20eca
Leonardo <leosimoes.engcomp@gmail.com>
2023-06-05 08:45:07 -0300

    HackerRank - Python - Introductions - Arithmetic Operators

---
+++ Introductions/Arithmetic-Operators/Arithmetic-Operators.py
@@ -0,0 +1,12 @@
+"""
+    Author: Leonardo Simoes
+    Date: 06/02/2023
+    HackerRank - Python - Arithmetic Operators
+"""
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    print(a+b)
+    print(a-b)
+    print(a*b)
