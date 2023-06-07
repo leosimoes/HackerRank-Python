f2c79024d088b3e80351d759501520694ccdcb3b
Leonardo <leosimoes.engcomp@gmail.com>
2023-06-07 12:48:00 -0300

    HackerRank - Python - Introductions - Write a function

---
+++ Introductions/Write-a-function/Write-a-function.py
@@ -0,0 +1,17 @@
+"""
+    Author: Leonardo Simoes
+    Date: 06/02/2023
+    HackerRank - Python - Introductions - Write a function
+"""
+
+
+def is_leap(year):
+    leap = False
+
+    if (year % 400 == 0) or (year % 4 == 0 and year % 100 != 0):
+        leap = True
+
+    return leap
+
+year = int(input())
+print(is_leap(year))
