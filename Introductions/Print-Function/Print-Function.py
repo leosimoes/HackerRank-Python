0794cf088168d9bad930876e7217614d052d04e7
Leonardo <leosimoes.engcomp@gmail.com>
2023-06-06 21:40:10 -0300

    HackerRank - Python - Introductions - Print Function

---
+++ Introductions/Print-Function/Print-Function.py
@@ -0,0 +1,12 @@
+"""
+    Author: Leonardo Simoes
+    Date: 06/02/2023
+    HackerRank - Python - Introductions - Print Function
+"""
+
+if __name__ == '__main__':
+    n = int(input())
+    s = ''
+    for i in range(1, n+1):
+        s = s + str(i)
+    print(s)
