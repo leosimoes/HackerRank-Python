b16a18e47ee85357b2386693de6c8bc3b1c414dd
Leonardo <leosimoes.engcomp@gmail.com>
2023-06-09 15:41:54 -0300

    HackerRank - Python - Introductions - Python Division

---
+++ Introductions/Python-Division/Python-Division.py
@@ -0,0 +1,11 @@
+"""
+    Author: Leonardo Simoes
+    Date: 06/08/2023
+    HackerRank - Python - Introductions - Python Division
+"""
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    print(a//b)
+    print(a/b)
