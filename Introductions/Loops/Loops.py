7345079291b525d8833046e403351ab9dc07bd7f
Leonardo <leosimoes.engcomp@gmail.com>
2023-06-10 08:14:00 -0300

    HackerRank - Python - Introductions - Loops

---
+++ Introductions/Loops/Loops.py
@@ -0,0 +1,10 @@
+"""
+    Author: Leonardo Simoes
+    Date: 06/10/2023
+    HackerRank - Python - Introductions - Loops
+"""
+
+if __name__ == '__main__':
+    n = int(input())
+    for i in range(0,n):
+        print(i**2)
